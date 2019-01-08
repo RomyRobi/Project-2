76369e5eac9c30ec284b95f97ed750958e3309fe
A DeBruler <dudeeoman@gmail.com>
2019-01-08 15:47:40 -0500

    Listings Count Functional

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // var randomScalingFactor = function() {
 //   return Math.round(Math.random() * 100);
 // };
+var chosenVar = 'listingsCount';
+
+var ctx = document.getElementById('polar-chart-area');
 
 var boroughs = [
   'Bronx',
@@ -10,13 +13,40 @@ var boroughs = [
   'Staten Island',
 ]
 
-function boroughFilter(borough){
-  return listing.borough = borough;
-}
-
 var chartColors = window.chartColors;
 var color = Chart.helpers.color;
+var backgroundColors = [
+  color(chartColors.red).alpha(0.5).rgbString(),
+  color(chartColors.orange).alpha(0.5).rgbString(),
+  color(chartColors.yellow).alpha(0.5).rgbString(),
+  color(chartColors.green).alpha(0.5).rgbString(),
+  color(chartColors.blue).alpha(0.5).rgbString(),
+];
+
+var chartTitle ='Choose a Category to View'
+var chartOptions = {
+  responsive: true,
+  legend: {
+    position: 'right',
+  },
+  title: {
+    display: true,
+    text: chartTitle
+  },
+  scale: {
+    ticks: {
+      beginAtZero: true
+    },
+    reverse: false
+  },
+  animation: {
+    animateRotate: false,
+    animateScale: true
+  }
+};
+
 var config = {
+  type: 'polarArea',
   data: {
     datasets: [{
       data: [
@@ -26,69 +56,67 @@ var config = {
         4,
         5,
       ],
-      backgroundColor: [
-        color(chartColors.red).alpha(0.5).rgbString(),
-        color(chartColors.orange).alpha(0.5).rgbString(),
-        color(chartColors.yellow).alpha(0.5).rgbString(),
-        color(chartColors.green).alpha(0.5).rgbString(),
-        color(chartColors.blue).alpha(0.5).rgbString(),
-      ],
+      backgroundColor: backgroundColors,
       label: 'My dataset' // for legend
     }],
     labels: boroughs
   },
-  options: {
-    responsive: true,
-    legend: {
-      position: 'right',
-    },
-    title: {
-      display: true,
-      text: 'Listing Data by Borough'
-    },
-    scale: {
-      ticks: {
-        beginAtZero: true
-      },
-      reverse: false
-    },
-    animation: {
-      animateRotate: false,
-      animateScale: true
-    }
-  }
+  options: chartOptions
 };
 
-window.onload = function() {
-  var ctx = document.getElementById('polar-chart-area');
-  window.myPolarArea = Chart.PolarArea(ctx, config);
-};
+var myChart = new Chart(ctx, config);
 
-document.getElementById('randomizeData').addEventListener('click', function() {
-  config.data.datasets.forEach(function(piece, i) {
-    piece.data.forEach(function(value, j) {
-      config.data.datasets[i].data[j] = randomScalingFactor();
-    });
-  });
-  window.myPolarArea.update();
-});
 
 const csv = "listings.csv"
 
-d3.csv(csv).then(function(listingsData)) {
+d3.csv(csv, function(listingsData) {
 
-    // Parse Data
-    newsData.forEach(function(data) {
-      data.accomodates = +data.accomodates;
-      data.price = +data.price;
-      data.minimum_night = +data.minimum_night;
-      data.review_scores_rating = +data.review_scores_rating;
-      data.review_scores_location = +data.review_scores_location;
+  // Code for abridging data. Only NAN returned for unknown reason
+  // var polarData = [];
+  // var currData ={};
+  //
+  // listingsData.forEach(function(data) {
+  //   currData.neighbourhood_group_cleansed = data.neighbourhood_group_cleansed;
+  //   currData.accommodates = +data.accommodates;
+  //   currData.price = parseFloat(data.price.replace(/[^0-9-.]/g, ''));
+  //   currData.minimum_nights = +data.minimum_nights;
+  //   currData.review_scores_rating = +data.review_scores_rating;
+  //   currData.review_scores_location = +data.review_scores_location;
+  //   console.log(currData);
+  //   polarData.push(currData);
+  //   console.log(polarData[polarData.length -1])
+  // });
+  //
+  // console.log(polarData);
+
+  // parse data
+  listingsData.forEach(function(data) {
+    data.neighbourhood_group_cleansed = data.neighbourhood_group_cleansed;
+    data.accommodates = +data.accommodates;
+    data.price = parseFloat(data.price.replace(/[^0-9-.]/g, ''));
+    data.minimum_nights = +data.minimum_nights;
+    data.review_scores_rating = +data.review_scores_rating;
+    data.review_scores_location = +data.review_scores_location;
+  });
+
+  console.log(listingsData);
+
+
+  document.getElementById('listingsCount').addEventListener('click', function() {
+    listingsCounts = [];
+    boroughs.forEach(borough => {
+      boroughData = listingsData.filter(listing => listing.neighbourhood_group_cleansed == borough);
+      boroughCount = boroughData.length;
+      listingsCounts.push(boroughCount);
     });
+    console.log(listingsCounts);
+    config.data.datasets[0].data = listingsCounts;
+    config.options.title.text = 'Number of Listings by Borough'
+    console.log(config);
+    myChart.update();
+  });
 
-    console.log(listingsData);
-
-}
+});
 
 
 // var colorNames = Object.keys(window.chartColors);
